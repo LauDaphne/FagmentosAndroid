966b7be78961c6e42ae36db4b86bd602db7d35a9
LauDaphne <laura.mp.job@gmail.com>
2020-01-11 20:22:36 +0100

    Finalización de la aplicación

--- app/src/main/java/com/example/fagmentosandroid/SeleccionandoImagenes.java
+++ app/src/main/java/com/example/fagmentosandroid/SeleccionandoImagenes.java
@@ -1,30 +1,62 @@
 package com.example.fagmentosandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
 
 public class SeleccionandoImagenes extends AppCompatActivity {
 
+    Button bttnRosa;
+    Button bttnAzahar;
+    Button bttnClavel;
+    Button bttnTulipan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seleccionando_imagenes);
-    }
 
-    public void seleccionarRosa(View view) {
-       getSupportFragmentManager().beginTransaction().replace(R.id.fragmentFlores, new RosaFragment()).commit();
-    }
-    public void seleccionarAzahar(View view) {
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentFlores, new AzaharFragment()).commit();
-    }
-    public void seleccionarClavel(View view) {
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentFlores, new ClavelFragment()).commit();
-    }
+        bttnRosa = findViewById(R.id.bttnRosa);
+        bttnAzahar = findViewById(R.id.bttnAzahar);
+        bttnClavel = findViewById(R.id.bttnClavel);
+        bttnTulipan = findViewById(R.id.bttnTulipan);
 
-    public void seleccionarTulipan(View view) {
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentFlores, new TulipanFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentFlores, new RosaFragment()).commit();
+
+
+        bttnRosa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentFlores, new RosaFragment()).commit();
+            }
+        });
+
+        bttnAzahar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentFlores, new AzaharFragment()).commit();
+            }
+        });
+
+        bttnClavel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentFlores, new ClavelFragment()).commit();
+            }
+        });
+
+        bttnTulipan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentFlores, new TulipanFragment()).commit();
+            }
+        });
+
     }
 }
